e43c2cb197c9fa3daf6ab6381b757b3f2a2b9092
Your Name <youremail@example.com>
2025-07-22 17:10:47 +0530

    Initial commit

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * themeslug functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/core-concepts/custom-functionality/
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ */
+
+// Remove default WordPress styles
+require_once get_theme_file_path( 'inc/remove-wp-defaults.php' );
+
+// Custom Blocks
+require_once get_theme_file_path( 'inc/custom-blocks.php' );
+
+// Block style variations
+require_once get_theme_file_path( 'inc/block-style-variations.php' );
+
+// Block output customizations
+require_once get_theme_file_path( 'inc/block-output-customizations.php' );
+
+// CSS
+require_once get_theme_file_path( 'inc/styles.php' );
+
+// Front-end JavaScript
+require_once get_theme_file_path( 'inc/js-scripts.php' );
+
+// Plugin: Gravity Forms
+require_once get_theme_file_path( 'inc/plugin-gravityforms.php' );
+
+// Plugin: WooCommerce
+require_once get_theme_file_path( 'inc/plugin-woocommerce.php' );
